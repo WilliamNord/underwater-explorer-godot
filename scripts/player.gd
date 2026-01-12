d775eae11cba862e22a9a5b9a5f20944f65e62e4
WilliamNord <williamnordberg08@gmail.com>
2026-01-12 13:23:14 +0100

    Revert "added detecting the player out of water"
    This reverts commit 2cf54b6923a805f1bd0a6261719f416e61d7acf2.

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,8 +12,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	
 	if is_swimming:
-		velocity.y = move_toward(velocity.y, 0, 500 * delta)
-		velocity.x = move_toward(velocity.x, 0, 500 * delta)
+		velocity.y = move_toward(velocity.y, 0, 200 * delta)
 	else:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
@@ -29,21 +28,13 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		if not is_swimming:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	if is_swimming:
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = -200
+			velocity.y = -150
 		elif Input.is_action_pressed("ui_down"):
-			velocity.y = 200
-		elif Input.is_action_pressed("ui_right"):
-			velocity.x = 200
-		elif Input.is_action_pressed("ui_left"):
-			velocity.x = -200
-			
-	if is_swimming:
-		velocity.x = clamp(velocity.x, -200, 200)
+			velocity.y = 150
 
 	move_and_slide()
 
@@ -52,12 +43,4 @@ func in_water_gravity():
 	is_swimming = true
 	if is_swimming == true:
 		print("grav is zero")
-
-func out_water_gravity():
-	print("player out of water")
-	is_swimming = false
-	if is_swimming == true:
-		print("grav is zero")
-	else:
-		print("grav is on")
 	
