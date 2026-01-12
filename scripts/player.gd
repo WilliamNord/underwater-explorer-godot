0fb1e2e4554d7862b9cb9648595dd0ff7a0e5c25
WilliamNord <williamnordberg08@gmail.com>
2026-01-12 13:44:31 +0100

    i have fixed a problem with my last commit with terminal commands
    i tried to dowload an assetLIB with github, but a lot of bugs popped up and i had to try to reset to last commit with i could not do with desktop so i used commands in the terminal and it seems to haev worked. i had to redo some changes but it looks like its working.
    i got help with git commands from @MathiasBenoni

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,29 +12,38 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	
 	if is_swimming:
-		velocity.y = move_toward(velocity.y, 0, 200 * delta)
+		velocity.y = move_toward(velocity.y, 0, 500 * delta)
+		velocity.x = move_toward(velocity.x, 0, 500 * delta)
 	else:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
-
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not is_swimming:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+
 		
 	if is_swimming:
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = -150
+			velocity.y = -200 
 		elif Input.is_action_pressed("ui_down"):
-			velocity.y = 150
+			velocity.y = 200 
+		elif Input.is_action_pressed("ui_right"):
+			velocity.x = 200 
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = -200
+	
+	if is_swimming:
+		velocity.x = clamp(velocity.x, -200, 200)
+
+
 
 	move_and_slide()
 
@@ -43,4 +52,11 @@ func in_water_gravity():
 	is_swimming = true
 	if is_swimming == true:
 		print("grav is zero")
-	
+		
+func out_water_gravity():
+	print("player out of water")
+	is_swimming = false 
+	if is_swimming == true: 
+		print("grav is zero")
+	else: 
+		print("grav is on")
--- scripts/water.gd
+++ scripts/water.gd
@@ -26,4 +26,4 @@ func _on_body_entered(body: Node2D) -> void:
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.
+		body.out_water_gravity()
