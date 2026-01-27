4fb2bd7cef7c4c37a0a61a17942950392253b31e
WilliamNord <williamnordberg08@gmail.com>
2026-01-27 13:40:48 +0100

    Jeg er ferdig med koden for automatisk bølgebredde

--- scenes/wave_shader.gd
+++ scenes/wave_shader.gd
@@ -1,17 +1,22 @@
 extends Area2D
 
-@export var wave_width: int = 100
+@export var wave_width: int = 3355
 @export var wave_height: int = 100
+@export var auto_width: bool = true
 
 @onready var main_wave: Sprite2D = $main_wave
 @onready var sub_wave: Sprite2D = $sub_wave
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	main_wave.region_rect = Rect2(0,0,wave_width,wave_height)
-	sub_wave.region_rect = Rect2(0,0,wave_width,wave_height)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	if auto_width:
+		var ocean = get_parent().get_node("Ocean")
+		global_position.x = ocean.get_node("CollisionShape2D").global_position.x
+		wave_width = ocean.get_node("CollisionShape2D").shape.size.x
+	
+	main_wave.region_enabled = true
+	main_wave.region_rect = Rect2(0, 0, wave_width, wave_height)
+	main_wave.centered = true
+	
+	sub_wave.region_enabled = true
+	sub_wave.region_rect = Rect2(0, 0, wave_width, wave_height)
+	sub_wave.centered = true
