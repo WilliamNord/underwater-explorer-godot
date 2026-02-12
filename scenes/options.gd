49576d0f8011bfea9b01ff1324539b27c28ec6fb
WilliamNord <williamnordberg08@gmail.com>
2026-02-12 09:42:27 +0100

    options button bug fixed

--- scenes/options.gd
+++ scenes/options.gd
@@ -2,12 +2,17 @@ extends Control
 
 @onready var button: Button = $Button
 
+
+@onready var play: Button = $button_container/Play
+@onready var options: Button = $button_container/Options
+@onready var quit: Button = $button_container/Quit
+
 @onready var button_container: VBoxContainer = $button_container
 
+
 func _ready() -> void:
-	#button_container = get_parent().get_node("button_container")
-	button_container.visible = false
-	
+	get_parent().get_node("button_container").visible = false
+
 func _on_button_pressed() -> void:
-	button_container.visible = true
+	get_parent().get_node("button_container").visible = true
 	queue_free()
