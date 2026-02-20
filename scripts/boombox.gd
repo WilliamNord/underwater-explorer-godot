34898eb94fef83d8d906fe290e9dca47694eb5fb
WilliamNord <williamnordberg08@gmail.com>
2026-02-20 20:13:09 +0100

    fikset musikk popup
    Jeg har fikset musikkpopup-en som lar deg skru av og på musikken og bytte sang når du vil. Jeg har endret koden helt til et mye enklere system for å kontrolere musikken.

--- scripts/boombox.gd
+++ scripts/boombox.gd
@@ -1,109 +1,119 @@
 extends Area2D
 
 @onready var label: Label = $Label
-var can_interact = false
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var songs: Array[AudioStream] = []
 var song_names: Array[String] = []
-var current_song_index: int = 0
-var is_paused: bool = false
 
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+var current_song_index: int = 0
+var is_playing: bool = true
+var can_interact: bool = false
+
 @onready var popup_scene = preload("res://scenes/music_popup.tscn")
-var current_popup = null
+var popup: MusicPopup = null
+
 
 func _ready() -> void:
 	load_songs()
 	
 	if songs.size() > 0:
-		audio_stream_player_2d.stream = songs[0]
-		audio_stream_player_2d.play()
-		
+		audio_player.stream = songs[0]
+		audio_player.play()
+	
 	label.visible = false
 	
+	audio_player.finished.connect(_on_song_finished)
 	create_popup()
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("interact") and can_interact == true:
-		toggle_popup()
-		
-	if not audio_stream_player_2d.playing and songs.size() > 0 and not is_paused:
-		next_song()
-		audio_stream_player_2d.play()
+
+func load_songs() -> void:
+	songs.append(preload("res://audio/music/geoffharvey-tropical-coconut-island-367671.mp3"))
+	song_names.append("Tropical Coconut Island")
+
+	songs.append(preload("res://audio/music/musictown-reggae-island-fun-109330.mp3"))
+	song_names.append("Musictown Reggae")
+
+	songs.append(preload("res://audio/music/tech_oasis-tropical-music-island-breeze-216656.mp3"))
+	song_names.append("Tech Oasis")
+
+	songs.append(preload("res://audio/music/treasure-beach-reggae-327069.mp3"))
+	song_names.append("Treasure Beach")
+
 
 func create_popup() -> void:
-	current_popup = popup_scene.instantiate()
-	current_popup.z_index = 100
-	current_popup.visible = false
+	popup = popup_scene.instantiate()
+	popup.visible = false
+	popup.z_index = 100
 	
-	get_tree().root.add_child(current_popup)
+	get_tree().root.add_child(popup)
 	
-	current_popup.set_song_list(song_names)
-	current_popup.song_changed.connect(_on_song_changed)
-	current_popup.play_pause_toggled.connect(_on_play_pause_toggled)
-	current_popup.close_requested.connect(close_popup)
+	popup.set_song_list(song_names)
+	popup.song_changed.connect(_on_song_changed)
+	popup.play_pause_toggled.connect(_on_play_pause_toggled)
+	popup.close_requested.connect(_on_close_requested)
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("interact") and can_interact:
+		toggle_popup()
+
 
 func toggle_popup() -> void:
-	if current_popup:
-		if current_popup.visible:
-			close_popup()
-		else:
-			current_popup.global_position = global_position + Vector2(-175, -350)
-			open_popup()
-
-func close_popup() -> void:
-	if current_popup:
-		current_popup.visible = false
-
-func open_popup() -> void:
-	if current_popup:
-		current_popup.visible = true
-
-func _on_song_changed(song_index: int) -> void:
-	current_song_index = song_index
-	audio_stream_player_2d.stream = songs[current_song_index]
-	audio_stream_player_2d.play()
-	is_paused = false
-
-func _on_play_pause_toggled(is_playing: bool) -> void:
-	if is_playing:
-		audio_stream_player_2d.play()
-		is_paused = false
+	if popup.visible:
+		popup.visible = false
 	else:
-		audio_stream_player_2d.stop()
-		is_paused = true
+		popup.global_position = global_position + Vector2(-175, -350)
+		popup.visible = true
+		popup.sync_state(current_song_index, is_playing)
+
+
+func _on_song_changed(index: int) -> void:
+	current_song_index = index
+	
+	audio_player.stop()
+	audio_player.stream = songs[current_song_index]
+	
+	if is_playing:
+		audio_player.play()
+
+
+func _on_play_pause_toggled(should_play: bool) -> void:
+	is_playing = should_play
+	
+	if is_playing:
+		audio_player.play()
+	else:
+		audio_player.stop()
+
+
+func _on_song_finished() -> void:
+	if not is_playing:
+		return
+	
+	current_song_index += 1
+	if current_song_index >= songs.size():
+		current_song_index = 0
+	
+	audio_player.stream = songs[current_song_index]
+	audio_player.play()
+	
+	if popup.visible:
+		popup.sync_state(current_song_index, is_playing)
+
+
+func _on_close_requested() -> void:
+	popup.visible = false
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		label.visible = true
 		can_interact = true
 
+
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		label.visible = false
 		can_interact = false
-		close_popup()
-
-func load_songs() -> void:
-	songs.append(preload("res://audio/music/geoffharvey-tropical-coconut-island-367671.mp3"))
-	song_names.append("Tropical Coconut Island")
-	
-	songs.append(preload("res://audio/music/musictown-reggae-island-fun-109330.mp3"))
-	song_names.append("Musictown Reggae")
-	
-	songs.append(preload("res://audio/music/tech_oasis-tropical-music-island-breeze-216656.mp3"))
-	song_names.append("Tech Oasis")
-	
-	songs.append(preload("res://audio/music/treasure-beach-reggae-327069.mp3"))
-	song_names.append("Treasure Beach")
-
-func next_song() -> void:
-	current_song_index += 1
-	if current_song_index >= songs.size():
-		current_song_index = 0
-		
-	audio_stream_player_2d.stream = songs[current_song_index]
-	
-	if current_popup and current_popup.visible:
-		current_popup.current_song_index = current_song_index
-		current_popup.update_ui()
+		popup.visible = false
--- scripts/music_popup.gd
+++ scripts/music_popup.gd
@@ -1,35 +1,40 @@
+class_name MusicPopup
 extends Control
 
 signal song_changed(song_index: int)
 signal play_pause_toggled(is_playing: bool)
 signal close_requested()
 
-var current_song_index: int = 0
 var song_list: Array[String] = []
+var current_song_index: int = 0
 var is_playing: bool = true
 
-
-@onready var title_label: Label = $Panel/MarginContainer/VBoxContainer/TitleLabel
 @onready var song_label: Label = $Panel/MarginContainer/VBoxContainer/SongLabel
-@onready var right: Button = $Panel/MarginContainer/VBoxContainer/HBoxContainer/right
-@onready var left: Button = $Panel/MarginContainer/VBoxContainer/HBoxContainer/left
+@onready var left_button: Button = $Panel/MarginContainer/VBoxContainer/HBoxContainer/left
+@onready var right_button: Button = $Panel/MarginContainer/VBoxContainer/HBoxContainer/right
 @onready var play_button: Button = $"Panel/MarginContainer/VBoxContainer/play button"
 @onready var close_button: Button = $"Panel/MarginContainer/VBoxContainer/close button"
 
 
 func _ready() -> void:
-	# Koble til signal handlers
-	left.pressed.connect(_on_left_pressed)
-	right.pressed.connect(_on_right_pressed)
+	left_button.pressed.connect(_on_left_pressed)
+	right_button.pressed.connect(_on_right_pressed)
 	play_button.pressed.connect(_on_play_pressed)
 	close_button.pressed.connect(_on_close_pressed)
 	
 	update_ui()
 
-func set_song_list(songs: Array[String]) -> void:
-	song_list = songs
-	current_song_index = 0
+
+func set_song_list(list: Array[String]) -> void:
+	song_list = list
 	update_ui()
+
+
+func sync_state(song_index: int, playing: bool) -> void:
+	current_song_index = song_index
+	is_playing = playing
+	update_ui()
+
 
 func _on_left_pressed() -> void:
 	current_song_index -= 1
@@ -39,6 +44,7 @@ func _on_left_pressed() -> void:
 	song_changed.emit(current_song_index)
 	update_ui()
 
+
 func _on_right_pressed() -> void:
 	current_song_index += 1
 	if current_song_index >= song_list.size():
@@ -47,28 +53,24 @@ func _on_right_pressed() -> void:
 	song_changed.emit(current_song_index)
 	update_ui()
 
+
 func _on_play_pressed() -> void:
-	if is_playing:
-		is_playing = false
-		play_pause_toggled.emit(is_playing)
-	else:
-		is_playing = true
-		play_pause_toggled.emit(!is_playing)
+	is_playing = !is_playing
+	play_pause_toggled.emit(is_playing)
 	update_ui()
+
 
 func _on_close_pressed() -> void:
 	close_requested.emit()
-	
+
 
 func update_ui() -> void:
-	# Oppdater play/pause button text
+	if song_list.size() > 0:
+		song_label.text = song_list[current_song_index]
+	else:
+		song_label.text = "No songs loaded"
+	
 	if is_playing:
 		play_button.text = "⏸ PAUSE"
 	else:
 		play_button.text = "▶ PLAY"
-	
-	# Oppdater song label (hvis den finnes)
-	if song_label and song_list.size() > 0:
-		song_label.text = song_list[current_song_index]
-	elif song_label:
-		song_label.text = "No songs loaded"
